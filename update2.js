ba1761e061b2283540f97f4665dc2fd5dcd4f438
CodeBreaker <47723333+Mirielle2004@users.noreply.github.com>
2020-11-16 01:26:31 -0800

    Create update2.js

---
+++ update2.js
@@ -0,0 +1,31 @@
+export * as mat from "https://cdn.jsdelivr.net/gh/Mirielle2004/modular-test@master/smodule.js";
+
+export const PI = 3.14;
+
+
+export class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+function hello() {
+    console.log("roygbiv");
+}
+
+export default function area(x, y) {
+    console.log(x * y);
+}
+
+
+export const Vector2 = (function() {
+    function Vector2(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    Vector2.prototype.add = function(v) {
+        return new Vector2(this.x + v.x, this.y + v.y)
+    }
+    return Vector2;
+})();
