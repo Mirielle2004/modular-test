6b2aa9cfee09beaa5f36a07d37b507317052dea4
CodeBreaker <47723333+Mirielle2004@users.noreply.github.com>
2020-11-16 08:00:59 -0800

    Update and rename bat.js to bat1.js

---
+++ bat1.js
@@ -0,0 +1,14 @@
+export const Bat = {
+    author: "Mirielle S.",
+    version: "0.0.2-alpha",
+    date: "",
+    license: "MIT"
+};
+
+
+
+// core
+export * as Utils from "../modular-test/lib/core/utils.js";
+export {Preload} from "../modular-test/lib/core/preloader.js";
+export {Scene} from "../modular-test/lib/core/scene.js";
+export {GameArea} from "../modular-test/lib/core/gameArea.js";
